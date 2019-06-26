023de36083b74ee24725e7d49d6c896c7fd5fa05
Robson Lawson <robsonlawson3@gmail.com>
2019-06-26 06:29:22 +0100

    Add basic discord bot setup

---
+++ src/main/java/com/apex/Discord/DSS/Events.java
@@ -0,0 +1,10 @@
+package com.apex.Discord.DSS;
+
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class Events extends ListenerAdapter
+{
+	public static final Events INSTANCE = new Events();
+
+	private Events() { }
+}
